79e0a461015f770079a6efc442c201cc3a88476f
blackbydefault <leroyabrother@gmail.com>
2025-01-10 10:45:15 -0500

    v3

--- src/main/java/org/example/AFRO.java
+++ src/main/java/org/example/AFRO.java
@@ -1,29 +1,53 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.*;
 
 import javax.security.auth.login.LoginException;
+/**
+ * @author Leroy A.
+ */
 
+
+
+
+/**
+ * JDA-5.2.2 IDS DISCORD BOT
+ * Main class to initialize bot.
+ */
 public class AFRO
 {
+    private final Dotenv config;
     private final ShardManager shardManager;
+
+    /**
+     * Loads environmental variables and builds the bot shard manager.
+     * @throws LoginException when bot token is not valid.
+     */
     public AFRO() throws LoginException
     {
-        String botToken = "NzA0Mzc4MzI3Mjg4MzgxNjEx.Gv3u4D.ZbeQvXRGHE3t8fVur0v3Rfn1gQ1d8S5LqKBRow";
+        config = Dotenv.configure().load();
+        String botToken = config.get("BOTTOKEN");
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(botToken);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.watching("threats."));
         shardManager = builder.build();
 
-
-
     }
-
+    /**
+     * Retrieves the bot shard manager
+     * @return the ShardManager instance for bot.
+     */
     public ShardManager getShardManager()
     {
         return shardManager;
+    }
+    public Dotenv getConfig()
+    {
+        return config;
+
     }
 
     public static void main(String[] args)
