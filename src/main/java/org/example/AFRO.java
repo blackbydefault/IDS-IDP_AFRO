69309c3327214885ed9774dd25a037ce3bef9645
blackbydefault <leroyabrother@gmail.com>
2025-01-09 22:32:52 -0500

    v1

---
+++ src/main/java/org/example/AFRO.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import net.dv8tion.jda.api.sharding.*;
+
+public class AFRO
+{
+    public AFRO()
+    {
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault("TOKEN");
+
+    }
+
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+}
