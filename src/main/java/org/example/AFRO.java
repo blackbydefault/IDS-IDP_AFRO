aca62aba5ed70374a184376614a0497aabb2b8bd
blackbydefault <119285013+blackbydefault@users.noreply.github.com>
2025-01-10 00:10:26 -0500

    V2

--- src/main/java/org/example/AFRO.java
+++ src/main/java/org/example/AFRO.java
@@ -1,18 +1,42 @@
 package org.example;
 
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.*;
+
+import javax.security.auth.login.LoginException;
 
 public class AFRO
 {
-    public AFRO()
+    private final ShardManager shardManager;
+    public AFRO() throws LoginException
     {
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault("TOKEN");
+        String botToken = "NzA0Mzc4MzI3Mjg4MzgxNjEx.Gv3u4D.ZbeQvXRGHE3t8fVur0v3Rfn1gQ1d8S5LqKBRow";
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(botToken);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.watching("threats."));
+        shardManager = builder.build();
+
+
 
     }
 
+    public ShardManager getShardManager()
+    {
+        return shardManager;
+    }
 
     public static void main(String[] args)
     {
+        try
+        {
+            AFRO bot = new AFRO();
+        }
+        catch (LoginException e)
+        {
+            System.out.println("[ERROR]: PROVIDED TOKEN IS NOT VALID.");
+
+        };
 
 
 
